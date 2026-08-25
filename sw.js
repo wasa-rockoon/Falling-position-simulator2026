2643531a0b4ae78b343d45e49744b411a8714d34
SCK132 <chrikotaki622@gmail.com>
2026-08-25 14:48:04 +0900

    feat: add Otsuki General Ground launch site

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var CACHE_PREFIX = 'wasa-predictor-';
-var CACHE_VERSION = '2838e14387a8';
+var CACHE_VERSION = 'fddc12776d20';
 var APP_CACHE_NAME = CACHE_PREFIX + 'app-' + CACHE_VERSION;
 var TILE_CACHE_NAME = CACHE_PREFIX + 'tiles-v1';
 var TILE_CACHE_LIMIT = 500;
